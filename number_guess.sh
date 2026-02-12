4df45baeb2e7b1003b5905adbdc90f15856798cc
sanjay786cell <sanjay1999a.d@gmail.com>
2026-02-12 07:26:39 -0500

    feat: added comment

--- number_guess.sh
+++ number_guess.sh
@@ -4,6 +4,7 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo "Enter your username:"
 read USERNAME
+#
 
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
 
