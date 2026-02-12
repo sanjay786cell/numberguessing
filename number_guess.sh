52724a1712e8b37fe018da62f3947b8d54a22a86
sanjay786cell <sanjay1999a.d@gmail.com>
2026-02-12 07:27:09 -0500

    feat: added new line

--- number_guess.sh
+++ number_guess.sh
@@ -7,7 +7,7 @@ read USERNAME
 #
 
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
-
+#
 if [[ -z $USER_ID ]]
 then
   echo "Welcome, $USERNAME! It looks like this is your first time here."
